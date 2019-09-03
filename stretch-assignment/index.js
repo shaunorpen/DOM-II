dcc4b816fd630ed481cc0f3920227afb0a9fa2ac
Shaun Orpen <shaunorpen@users.noreply.github.com>
2019-09-03 15:57:49 +0100

    WIP

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -9,7 +9,8 @@ rockets.forEach(
 
 rockets.forEach(
     rocket => rocket.addEventListener('mousedown', (e) => {
-        rockets.forEach(rocket => rocket.style.marginLeft = 0);
-        e.target.style.marginLeft = '100px';
+        // rockets.forEach(rocket => rocket.style.marginLeft = 0);
+        // e.target.style.marginLeft = parseInt(e.target.style.marginLeft) + 10 + 'px';
+        console.log(e);
     }
 ));
